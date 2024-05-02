f33a98793a6a25e432d0abb1028aa1c4b6ef7dc2
Yoav TC <yoavtc2004@gmail.com>
2024-05-02 13:51:24 +0300

    Added sliding mechanic
    Also imported DoTween

--- Unity/prototype/untitled-colour-game-v1/Assets/PlayerMovement.cs
+++ Unity/prototype/untitled-colour-game-v1/Assets/PlayerMovement.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using DG.Tweening;
 using NaughtyAttributes;
 using TMPro;
 using UnityEngine;
@@ -29,36 +31,12 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float wallJumpForce;
     [ReadOnly] [SerializeField] private bool mountedRightWall, mountedLeftWall, isWallJumping;
 
-    #region Value Testing
-    public void SetDecelRate(float newValue) => decelerationRate = newValue;
-    public void SetAirDecelRate(float newValue) => airDecelerationRateMultiplier = newValue;
-    public void SetMaxSpeed(float newValue) => maxSpeed = newValue;
-    public void SetJumpForce(float newValue) => jumpForce = newValue;
-    public void SetWallJumpForce(float newValue) => wallJumpForce = newValue;
-
-    [SerializeField] private TMP_Text statsDisplay;
-    
-    private void UpdateStatsDisplay()
-    {
-        statsDisplay.text = "decel rate: " + decelerationRate
-                                           + "\nair decel rate: " + airDecelerationRateMultiplier
-                                           + "\nmax speed: " + maxSpeed
-                                           + "\njump force: " + jumpForce
-                                           + "\nwall jump force: " + wallJumpForce;
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            SceneManager.LoadScene(0);
-        }
-        if (Input.GetKeyDown(KeyCode.Y))
-        {
-            GUIUtility.systemCopyBuffer = statsDisplay.text;
-        }
-        if (Input.GetKeyDown(KeyCode.Z))
-        {
-            transform.position = new Vector3(50, 6, 0);
-        }
-    }
-    #endregion
+    [Header("Sliding")] 
+    [ReadOnly] [SerializeField] private bool isSliding;
+    [ReadOnly] [SerializeField] private bool canGetUp;
+    [SerializeField] private float slidingDecelerationRateMultiplier;
+    [SerializeField] private float slideDurationMax, slideDurationMin;
+    private Coroutine slideCoroutine;
     
     private void Start()
     {
@@ -67,10 +45,8 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        UpdateStatsDisplay();
-        
         //Horizontal movement
-        if (transform.position.x < 56.8f)
+        if (transform.position.x < 56.8f && !isSliding)
         {
             moveInput = Input.GetAxisRaw("Horizontal");
         }
@@ -95,9 +71,16 @@ public class PlayerMovement : MonoBehaviour
         }
         
         //Sliding
-        if (isGrounded && Input.GetButtonDown("Slide"))
+        if (isGrounded && Input.GetButtonDown("Slide") && !isSliding)
+        { ;
+            slideCoroutine = StartCoroutine(Slide());
+            transform.GetChild(1).GetComponent<SpriteRenderer>().color = Color.red;
+            //transform.DORotate(new Vector3(0, 0, 70), 0.1f);
+        }
+
+        if (!Input.GetButton("Slide") && isSliding && canGetUp)
         {
-            //Implement slide state
+            GetUpFromSlide();
         }
 
         //Testing death event
@@ -110,11 +93,8 @@ public class PlayerMovement : MonoBehaviour
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, wallGroundLayer);
         
         Vector3 transformPosition = transform.position;
-        mountedRightWall =
-            Physics2D.OverlapPoint(transformPosition + new Vector3(0.25f, 0f, 0f), wallGroundLayer);
-        
-        mountedLeftWall =
-            Physics2D.OverlapPoint(transformPosition + new Vector3(-0.25f, 0f, 0f), wallGroundLayer);
+        mountedRightWall = Physics2D.OverlapPoint(transformPosition + new Vector3(0.25f, 0f, 0f), wallGroundLayer);
+        mountedLeftWall = Physics2D.OverlapPoint(transformPosition + new Vector3(-0.25f, 0f, 0f), wallGroundLayer);
 
         if (isGrounded) isWallJumping = false;
         if (Mathf.Abs(moveInput) > 0) isWallJumping = false;
@@ -126,7 +106,10 @@ public class PlayerMovement : MonoBehaviour
         if (moveInput == 0 && !isWallJumping)
         {
             float deceleration = decelerationRate;
+            
             if (!isGrounded) deceleration = decelerationRate * airDecelerationRateMultiplier;
+            if (isGrounded && isSliding) deceleration = decelerationRate * slidingDecelerationRateMultiplier;
+            
             rb.velocity = new Vector2(Mathf.Lerp(rb.velocity.x, 0, deceleration), rb.velocity.y);
         }
 
@@ -144,5 +127,27 @@ public class PlayerMovement : MonoBehaviour
     {
         rb.velocity = Vector2.zero;
         transform.position = new Vector3(-7f, 1f, 0f);
+    }
+
+    private IEnumerator Slide()
+    {
+        canGetUp = false;
+        isSliding = true;
+        float timeDiff = slideDurationMax - slideDurationMin;
+        
+        yield return new WaitForSeconds(slideDurationMin);
+        canGetUp = true;
+        
+        yield return new WaitForSeconds(timeDiff);
+        if (isSliding) GetUpFromSlide();
+    }
+
+    private void GetUpFromSlide()
+    {
+        StopCoroutine(slideCoroutine);
+        canGetUp = false;
+        isSliding = false;
+        transform.GetChild(1).GetComponent<SpriteRenderer>().color = Color.white;
+        //transform.DORotate(Vector3.zero, 0.1f);
     }
 }
