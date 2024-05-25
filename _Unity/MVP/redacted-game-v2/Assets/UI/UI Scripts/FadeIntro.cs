30b2a2cb8f47d487f7cfa66406bf87dd8c442620
Yoav TC <yoavtc2004@gmail.com>
2024-05-25 23:06:58 +0300

    Added animated intro scene

--- _Unity/MVP/redacted-game-v2/Assets/UI/UI Scripts/FadeIntro.cs
+++ _Unity/MVP/redacted-game-v2/Assets/UI/UI Scripts/FadeIntro.cs
@@ -1,17 +1,34 @@
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 using Image = UnityEngine.UI.Image;
 
 public class FadeIntro : MonoBehaviour
 {
     [SerializeField] private Image fade;
+    [SerializeField] private VideoPlayer videoPlayer;
 
     private void Start()
     {
-        fade.DOFade(0, 5f).SetDelay(1f).SetEase(Ease.InOutSine).OnComplete(() =>
+        // fade.DOFade(0, 5f).SetDelay(1f).SetEase(Ease.InOutSine).OnComplete(() =>
+        // {
+        //     SceneManager.LoadScene(1);
+        // });
+        videoPlayer.Prepare();
+        videoPlayer.prepareCompleted += source =>
         {
-            SceneManager.LoadScene(1);
-        });
+            fade.DOFade(0, 0.75f).SetDelay(1f).OnComplete(() =>
+            {
+                source.Play();
+            });
+        };
+        videoPlayer.loopPointReached += source =>
+        {
+            fade.DOFade(1, 2f).SetDelay(1f).OnComplete(() =>
+            {
+                SceneManager.LoadScene(1);
+            });
+        };
     }
 }
