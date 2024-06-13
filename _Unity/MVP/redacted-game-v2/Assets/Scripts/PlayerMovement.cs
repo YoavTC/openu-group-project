5cc92a45b9b1d6096a8c60e79b927e825bee1b82
Yoav TC <yoavtc2004@gmail.com>
2024-06-13 09:05:51 +0300

    implemented jump animation

--- _Unity/MVP/redacted-game-v2/Assets/Scripts/PlayerMovement.cs
+++ _Unity/MVP/redacted-game-v2/Assets/Scripts/PlayerMovement.cs
@@ -128,6 +128,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void Jump()
     {
+        animator.SetTrigger("jump");
         rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Force);
     }
     
