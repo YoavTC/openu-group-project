6550a462d93c1bb18b795d9369e0d3dffed584af
Yoav TC <yoavtc2004@gmail.com>
2024-06-19 17:13:50 +0300

    Finished Predator System + added configurable speed zones

--- _Unity/MVP/redacted-game-v2/Assets/Scripts/PredatorSystem.cs
+++ _Unity/MVP/redacted-game-v2/Assets/Scripts/PredatorSystem.cs
@@ -1,91 +1,55 @@
-using System;
-using DG.Tweening;
-using Unity.Mathematics;
+using Pathfinding;
 using UnityEngine;
-using UnityEngine.Splines;
-using Spline = UnityEngine.Splines.Spline;
 
 public class PredatorSystem : MonoBehaviour
 {
     [Header("Components")]
-    private Spline splinePath;
     [SerializeField] private Transform droneObject;
-    [SerializeField] private Transform playerTransform;
     [SerializeField] private Transform debugObject;
+    [SerializeField] private AIPath droneAI;
+    private Transform playerTransform;
 
     [Header("Drone Settings")] 
     [SerializeField] private float droneSpeed;
+    [SerializeField] private float droneUpSpeed;
+    private float lastSpeed;
+    [SerializeField] private float droneReturnSpeed;
+    [SerializeField] private float droneReturnDistance;
     [SerializeField] private float droneKillDistance;
-    [SerializeField] private float droneSeeDistance;
-    private BezierKnot closestKnot;
-
-    #region old
-
-    // void Start()
-    // {
-    //     splinePath = GetComponent<SplineContainer>().Spline;
-    //     if (playerTransform == null) playerTransform = FindObjectOfType<PlayerMovement>().transform;
-    //     
-    //     //InvokeRepeating(nameof(UpdateClosestKnot), 0f, 0.5f);
-    // }
-    //
-    // void Update()
-    // {
-    //     UpdateClosestKnot();
-    //     debugObject.localPosition = closestKnot.Position;
-    // }
-    //
-    // private void UpdateClosestKnot()
-    // {
-    //     BezierKnot[] knots = splinePath.ToArray();
-    //     closestKnot = knots[0];
-    //     float knotDistance = KnotDroneDistance(knots[0]);
-    //     for (int i = 0; i < knots.Length; i++)
-    //     {
-    //         float tempDistance = KnotDroneDistance(knots[i]);
-    //         if (tempDistance < knotDistance)
-    //         {
-    //             knotDistance = tempDistance;
-    //             closestKnot = knots[i];
-    //         }
-    //     }
-    // }
-    //
-    // private float KnotDroneDistance(BezierKnot knot)
-    // {
-    //     return Vector3.Distance(knot.Position, droneObject.position);
-    // }
-
-    #endregion
 
     private void Start()
     {
-        splinePath = GetComponent<SplineContainer>().Spline;
         if (playerTransform == null) playerTransform = FindObjectOfType<PlayerMovement>().transform;
+        droneAI.maxSpeed = droneSpeed;
     }
-
+    
     private void Update()
     {
-        //Update debug object
-        //debugObject.position = playerTransform.position;
-        
-        //Update drone
-        float3 closestPointFloat3;
-        SplineUtility.GetNearestPoint(splinePath, playerTransform.localPosition, out closestPointFloat3, out float t);
-        Vector3 closestPoint = closestPointFloat3;
-        debugObject.position = splinePath.EvaluatePosition(t);
-        
-        droneObject.DOMove(debugObject.position, droneSpeed).SetEase(Ease.InOutSine);
-        //droneObject.position = Vector3.Lerp(droneObject.position, debugObject.position, droneSpeed);
-        //droneObject.position = Vector3.MoveTowards(droneObject.position, debugObject.position, droneSpeed);
+        float dronePlayerDistance = DronePlayerDistance();
 
-        float dronePlayerDistance = Vector3.Distance(droneObject.position, playerTransform.position);
+        //Move drone
+        droneAI.maxSpeed = (dronePlayerDistance > droneReturnDistance) ? droneReturnSpeed : droneSpeed;
         
-        
-
+        //Checks
         if (dronePlayerDistance < droneKillDistance)
         {
             Debug.Log("Player got caught!");
         }
+    }
+
+    private float DronePlayerDistance()
+    {
+        return Vector2.Distance(droneObject.position, playerTransform.position);
+    }
+
+    public void EnterSpeedZone(float newSpeed)
+    {
+        lastSpeed = droneSpeed;
+        droneSpeed = (newSpeed / 100) * droneSpeed;
+    }
+
+    public void ExitSpeedZone()
+    {
+        droneSpeed = lastSpeed;
     }
 }
