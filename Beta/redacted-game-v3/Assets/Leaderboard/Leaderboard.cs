7f455e3725d7c83e8e0ec01d3f6a2ca103cb27e7
Yoav TC <yoavtc2004@gmail.com>
2024-07-08 20:22:32 +0300

    Removed old leaderboard backend

--- Beta/redacted-game-v3/Assets/Leaderboard/Leaderboard.cs
+++ Beta/redacted-game-v3/Assets/Leaderboard/Leaderboard.cs
@@ -1,8 +1,6 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Dan.Main;
 using DG.Tweening;
 using LootLocker.Requests;
 using NaughtyAttributes;
--- Beta/redacted-game-v3/Assets/UI/UI Scripts/ScoreDisplay.cs
+++ Beta/redacted-game-v3/Assets/UI/UI Scripts/ScoreDisplay.cs
@@ -1,10 +1,6 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using NaughtyAttributes;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class ScoreDisplay : MonoBehaviour
 {
