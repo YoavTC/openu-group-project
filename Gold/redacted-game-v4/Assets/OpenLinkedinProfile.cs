5ba3b08734048796b1c3145547bd16c8cc874e2f
Yoav TC <yoavtc2004@gmail.com>
2024-07-17 22:27:13 +0300

    Added about scene

---
+++ Gold/redacted-game-v4/Assets/OpenLinkedinProfile.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLinkedinProfile : MonoBehaviour
+{
+    public void OpenInbarURL() => Application.OpenURL("https://www.linkedin.com/in/inbar-gal-bb1125308/");
+    public void OpenYoavURL() => Application.OpenURL("https://www.linkedin.com/in/yoav-trachtman-cohen/");
+    public void OpenOhadURL() => Application.OpenURL("https://www.linkedin.com/in/ohad-dori/");
+    public void OpenAlexURL() => Application.OpenURL("https://www.linkedin.com/in/alex-yakovlev-430124308/");
+}
