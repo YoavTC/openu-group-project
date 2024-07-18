274fb5e0c7548f6bb4c0722253d6e4b586c3dfda
Yoav TC <yoavtc2004@gmail.com>
2024-07-18 09:17:14 +0300

    Added very important thing
    🐕

--- Gold/redacted-game-v4/Assets/OpenLinkedinProfile.cs
+++ Gold/redacted-game-v4/Assets/OpenLinkedinProfile.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OpenLinkedinProfile : MonoBehaviour
 {
@@ -8,4 +10,23 @@ public class OpenLinkedinProfile : MonoBehaviour
     public void OpenYoavURL() => Application.OpenURL("https://www.linkedin.com/in/yoav-trachtman-cohen/");
     public void OpenOhadURL() => Application.OpenURL("https://www.linkedin.com/in/ohad-dori/");
     public void OpenAlexURL() => Application.OpenURL("https://www.linkedin.com/in/alex-yakovlev-430124308/");
+
+    private int gInput;
+    private bool gMode = false;
+    [SerializeField] private Image gingerUIElement;
+    [SerializeField] private TMP_Text yoavDesc;
+    
+    private void Update()
+    {
+        if (!gMode && Input.GetKeyDown(KeyCode.G))
+        {
+            gInput++;
+            if (gInput >= 20)
+            {
+                gMode = true;
+                gingerUIElement.enabled = true;
+                yoavDesc.text = "Ginger. My dog (:";
+            }
+        }
+    }
 }
