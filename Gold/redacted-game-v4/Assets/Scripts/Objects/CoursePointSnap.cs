6b5fb7afa4b8565beb6e5cfeb9d2d2dd59162e0b
Yoav TC <yoavtc2004@gmail.com>
2024-07-19 17:07:13 +0300

    Added offset for climbing snapping points

--- Gold/redacted-game-v4/Assets/Scripts/Objects/CoursePointSnap.cs
+++ Gold/redacted-game-v4/Assets/Scripts/Objects/CoursePointSnap.cs
@@ -2,14 +2,17 @@ using UnityEngine;
 
 public class CoursePointSnap : MonoBehaviour
 {
+    [SerializeField] private float xOffset;
     void Start()
     {
         LayerMask groundLayers = LayerMask.GetMask("NonJumpable", "WallGround");
         Vector2 centerPos = transform.position;
         RaycastHit2D rightPoint = Physics2D.Raycast(centerPos, Vector2.right, 100f, groundLayers);
         RaycastHit2D leftPoint = Physics2D.Raycast(centerPos, Vector2.left, 100f, groundLayers);
-        
-        Vector2 wallPoint = Vector2.Distance(rightPoint.point, centerPos) < Vector2.Distance(leftPoint.point, centerPos) ? rightPoint.point : leftPoint.point;
-        transform.position = wallPoint;
+
+        int isRightCloser = Vector2.Distance(rightPoint.point, centerPos) < Vector2.Distance(leftPoint.point, centerPos) ? 1 : -1;
+        Vector2 wallPoint = isRightCloser == 1 ? rightPoint.point : leftPoint.point;
+        Vector2 offset = new Vector2(xOffset * (isRightCloser * -1), 0);
+        transform.position = wallPoint + offset;
     }
 }
