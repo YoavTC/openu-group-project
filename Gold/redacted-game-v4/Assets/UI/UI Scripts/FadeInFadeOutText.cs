5bf2ca12622d956d75678ba985c627b825894984
Yoav TC <yoavtc2004@gmail.com>
2024-07-21 20:41:28 +0300

    Intro sequence changes

---
+++ Gold/redacted-game-v4/Assets/UI/UI Scripts/FadeInFadeOutText.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+
+public class FadeInFadeOutText : MonoBehaviour
+{
+    [SerializeField] private float fadeInDuration, fadeOutDuration, fadeOutDelayDuration;
+    [SerializeField] private Vector2 scales;
+    IEnumerator Start()
+    {
+        TMP_Text text = GetComponent<TMP_Text>();
+        text.DOFade(0, 0);
+        Color color = text.color;
+        color.a = 0f;
+        text.color = color;
+        text.rectTransform.localScale *= scales.x;
+        
+        text.DOFade(1, fadeInDuration);
+        text.rectTransform.DOScale(new Vector2(scales.y, scales.y), fadeInDuration + fadeOutDelayDuration + fadeOutDuration).SetEase(Ease.InOutSine);
+        yield return HelperFunctions.GetWait(fadeInDuration + fadeOutDelayDuration);
+        text.DOFade(0, fadeOutDuration);
+    }
+}
