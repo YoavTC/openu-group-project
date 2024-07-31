5a98c77cabc7536aacc25120f9fd8866c3bed472
Yoav TC <yoavtc2004@gmail.com>
2024-07-31 17:23:57 +0300

    Added glass breaking audio

--- Gold/redacted-game-v4/Assets/Scripts/Objects/BreakableGlass.cs
+++ Gold/redacted-game-v4/Assets/Scripts/Objects/BreakableGlass.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class BreakOnContact : MonoBehaviour
 {
     private ParticleSystem breakingParticle;
     private SpriteRenderer spriteRenderer;
+    private AudioSource audioSource;
     private bool played;
 
     private void Start()
@@ -15,6 +17,7 @@ public class BreakOnContact : MonoBehaviour
         
         spriteRenderer = transform.GetChild(0).GetComponent<SpriteRenderer>();
         breakingParticle = GetComponent<ParticleSystem>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -27,6 +30,9 @@ public class BreakOnContact : MonoBehaviour
         played = true;
         spriteRenderer.enabled = false;
         breakingParticle.Play();
+        float randomPitch = Random.Range(1.1f, 1.5f);
+        audioSource.pitch = randomPitch;
+        audioSource.Play();
     }
 
     private void OnParticleSystemStopped()
